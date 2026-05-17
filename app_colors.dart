560a2ba9ee840f3e4be9a531b22917608f88d04b
ibrahemabuhantash <ibrahemabuhantash@gmail.com>
2026-05-17 14:33:53 +0300

    Add files via upload

---
+++ app_colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary = Color(0xFF0E7A6B);
+  static const Color secondary = Color(0xFFFFA726);
+  static const Color background = Color(0xFFF7F8FA);
+  static const Color inputFill = Color(0xFFF2F4F7);
+  static const Color textDark = Color(0xFF1A1A1A);
+  static const Color textLight = Color(0xFF6B7280);
+  static const Color danger = Color(0xFFE53935);
+}
